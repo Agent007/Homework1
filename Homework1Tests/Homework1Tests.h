60e1ee3aad55700e67c3b64b51d9e9fc05f946d9
Jeff Lam <sha0lin@alumni.carnegiemellon.edu>
2012-02-10 17:31:30 -0500

    Initial Commit

---
+++ Homework1Tests/Homework1Tests.h
@@ -0,0 +1,13 @@
+//
+//  Homework1Tests.h
+//  Homework1Tests
+//
+//  Created by Jeffrey Lam on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Homework1Tests : SenTestCase
+
+@end
---
+++ Homework1Tests/Homework1Tests.m
@@ -0,0 +1,32 @@
+//
+//  Homework1Tests.m
+//  Homework1Tests
+//
+//  Created by Jeffrey Lam on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Homework1Tests.h"
+
+@implementation Homework1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Homework1Tests");
+}
+
+@end
